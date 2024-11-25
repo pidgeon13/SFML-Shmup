3a075ca31dc0a399a03415d6dd019a6cb2d73b65
pidgeon13 <pidgeon13@googlemail.com>
2024-11-25 19:49:06 +0000

    Pull engine code into library for unit testing

--- Tests/GeometryTests.cpp
+++ Tests/GeometryTests.cpp
@@ -2,7 +2,7 @@
 #include "Geometry.h"
 
 TEST(GeometryTests, Length2) {
-	sf::Vector2f vector(16, 9);
-	//float length2 = Geometry::Length2(vector);
-	//EXPECT_EQ(length2, 25);
+	sf::Vector2f vector(4, 3);
+	float length2 = Geometry::Length2(vector);
+	EXPECT_EQ(length2, 25);
 }
